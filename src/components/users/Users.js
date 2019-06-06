4e0db05cd7d7b9fee4ed163814431ac069bdf3f6
rosowskimik <m.rosowski1@wp.pl>
2019-06-06 19:35:26 +0200

    Fetching data from Github API

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,40 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem';
 
-class Users extends Component {
-  state = {
-    users: [
-      {
-        id: 49301780,
-        login: 'rosowskimik',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/49301780?v=4',
-        html_url: 'https://github.com/rosowskimik'
-      },
-      {
-        id: 1,
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo'
-      },
-      {
-        id: 25,
-        login: 'Caged',
-        avatar_url: 'https://avatars3.githubusercontent.com/u/25?v=4',
-        html_url: 'https://github.com/Caged'
-      }
-    ]
-  };
-
-  render() {
-    return (
-      <div style={userStyle}>
-        {this.state.users.map(user => (
-          <UserItem key={user.id} user={user} />
-        ))}
-      </div>
-    );
-  }
-}
+const Users = props => {
+  return (
+    <div style={userStyle}>
+      {props.users.map(user => (
+        <UserItem key={user.id} user={user} />
+      ))}
+    </div>
+  );
+};
 
 const userStyle = {
   display: 'grid',
