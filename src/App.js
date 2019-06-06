bd432450184b36055045c3fec74648a78ddb6ccb
rosowskimik <m.rosowski1@wp.pl>
2019-06-06 18:30:06 +0200

    Basic Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello There!</h1>
+      <Navbar />
     </div>
   );
 }
