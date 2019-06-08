64d96651e794f1ca0fb43bdddcd2f50b0aca7326
rosowskimik <m.rosowski1@wp.pl>
2019-06-08 13:51:12 +0200

    Displaying user data

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,4 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
 
 class User extends Component {
   componentDidMount() {
@@ -6,9 +9,16 @@ class User extends Component {
     console.log(this.props.match.params.login);
   }
 
+  static propTypes = {
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired,
+    loading: PropTypes.bool.isRequired
+  };
+
   render() {
     const {
       name,
+      company,
       avatar_url,
       location,
       bio,
@@ -23,7 +33,75 @@ class User extends Component {
     } = this.props.user;
     const { loading } = this.props;
 
-    return <div>{name}</div>;
+    if (loading) return <Spinner />;
+
+    return (
+      <Fragment>
+        <Link to='/' className='btn btn-light'>
+          Back To Search
+        </Link>
+        Hireable:{' '}
+        {hireable ? (
+          <i className='fas fa-check text-success' />
+        ) : (
+          <i className='fas fa-times-circle text-danger' />
+        )}
+        <div className='card grid-2'>
+          <div className='all-center'>
+            <img
+              src={avatar_url}
+              alt=''
+              className='round-img'
+              style={{ width: '150px' }}
+            />
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a
+              href={html_url}
+              className='btn btn-dark my-1'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Visit Github Profile
+            </a>
+            <p>
+              <strong>Username: </strong>
+              {login}
+            </p>
+            {company && (
+              <Fragment>
+                <p>
+                  <strong>Company: </strong>
+                  {company}
+                </p>
+              </Fragment>
+            )}
+            {blog && (
+              <Fragment>
+                <p>
+                  <strong>Website: </strong>
+                  {blog}
+                </p>
+              </Fragment>
+            )}
+          </div>
+        </div>
+        <div className='card text-center'>
+          <div className='badge badge-danger'>Followers: {followers}</div>
+          <div className='badge badge-success'>Following: {following}</div>
+          <div className='badge badge-dark'>Public Repos: {public_repos}</div>
+          <div className='badge badge-light'>Public Gists: {public_gists}</div>
+        </div>
+      </Fragment>
+    );
   }
 }
 
